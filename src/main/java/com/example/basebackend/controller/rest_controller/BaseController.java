d5ef61e1fc44e90bfa76b606908c3f4c2741f1bb
phnam3 <phnam3@cmcglobal.vn>
2022-07-07 10:32:58 +0700

    refactor! (Pagination): Changed pagination to use pageable class for better practice

--- src/main/java/com/example/basebackend/controller/rest_controller/BaseController.java
+++ src/main/java/com/example/basebackend/controller/rest_controller/BaseController.java
@@ -9,7 +9,9 @@ import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -64,20 +66,8 @@ public abstract class BaseController<T extends BaseDto> {
     }
 
     @GetMapping(value = "/findAll")
-    public final ResponseEntity<?> findAll(@RequestParam(value = "page", required=false) Integer page,
-                                           @RequestParam(value = "size", required=false) Integer size,
-                                           @RequestParam(value = "order", required=false) String order,
-                                           @RequestParam(value = "by", required=false) String by){
-        Sort sort = createSort(order,by);
-        PageRequest pageRequest = createPageRequest(page,size,sort);
-        Iterable<T> result;
-        if(sort == null && pageRequest == null){
-            result = baseService.findAll();
-        } else if (pageRequest != null){
-            result = baseService.findAll(pageRequest);
-        } else {
-            result = baseService.findAll(sort);
-        }
+    public final ResponseEntity<?> findAll(@PageableDefault Pageable pageable){
+        Iterable<T> result = baseService.findAll(pageable);
         getLogger().info("Result: {}", result);
         return BaseResponseDto.builder().httpStatus(HttpStatus.OK).data(result).build().toResponseEntity();
     }
@@ -129,33 +119,5 @@ public abstract class BaseController<T extends BaseDto> {
         getLogger().info("Param id={}", ids);
         baseService.deleteAll(ids);
         return ResponseEntity.noContent().build();
-    }
-
-    private Sort createSort(String order, String by){
-        getLogger().info("Params: order={}, by={}", order,by);
-        Sort sort = null;
-        if(order != null && !order.isEmpty() && by != null && !by.isEmpty()){
-            if(order.equalsIgnoreCase("ASC")){
-                sort = Sort.by(Sort.Direction.ASC, by);
-            } else if(order.equalsIgnoreCase("DESC")){
-                sort = Sort.by(Sort.Direction.DESC, by);
-            }
-            getLogger().info("Sort: {}", sort);
-        }
-        return sort;
-    }
-
-    private PageRequest createPageRequest(Integer page, Integer size, Sort sort){
-        getLogger().info("Params: page={}, size={}",page,size);
-        PageRequest pageRequest = null;
-        if(page != null && size != null){
-            if(sort != null) {
-                pageRequest = PageRequest.of(page, size, sort);
-            } else {
-                pageRequest = PageRequest.of(page,size);
-            }
-            getLogger().info("PageRequest: {}", pageRequest);
-        }
-        return pageRequest;
     }
 }
